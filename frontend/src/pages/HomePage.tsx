b374d6f420c447d4333151449fdeffae42c8472c
AvhiMaz <avhimazumder5@outlook.com>
2024-07-12 16:06:33 +0530

    remove class from landing page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,24 +1,23 @@
-import Container from "@/components/Container"
-import { Hero, HeroSubtitle, HeroTitle } from "@/components/Hero"
-import { Link } from "react-router-dom"
-
+import Container from "@/components/Container";
+import { Hero, HeroSubtitle, HeroTitle } from "@/components/Hero";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <Container>
-          <Hero
-          >
-            <HeroTitle>
-            Start Generating Viral Tweets Today <br /> Let <Link  to="/" className="hover:bg-zinc-400 inline-block text-transparent bg-clip-text">viralxpost</Link> Be Your Guide.
-            </HeroTitle>
-            <HeroSubtitle>
-              Meet the new standard for modern software development. <br/>
-              Streamline issues, sprints, and product roadmaps.
-            </HeroSubtitle>
-            {/* <img src="/img/hero.webp" alt="Hero image"/> */}
-          </Hero>
-        </Container>
-  )
-}
+      <Hero>
+        <HeroTitle>
+          Start Generating Viral Tweets Today <br /> Let{" "}
+          <Link to="/">viralxpost</Link> Be Your Guide.
+        </HeroTitle>
+        <HeroSubtitle>
+          Meet the new standard for modern software development. <br />
+          Streamline issues, sprints, and product roadmaps.
+        </HeroSubtitle>
+        {/* <img src="/img/hero.webp" alt="Hero image"/> */}
+      </Hero>
+    </Container>
+  );
+};
 
-export default HomePage
+export default HomePage;
