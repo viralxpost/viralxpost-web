01ae8726844c3f344f1cdc46fb3adb034e827bbc
PawanK06 <pawan06kumar2003@gmail.com>
2024-07-12 14:34:21 +0530

    add shadcn and poppins font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from "./components/ui/button";
+
+
 function App() {
   return (
     <>
       <div className="text-9xl text-rose-500">viralXpost</div>
+      <Button>
+        Click
+      </Button>
+
     </>
   );
 }
