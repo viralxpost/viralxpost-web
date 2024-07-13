6b3d61b66b3473172451f18534e4a9a237ce0647
AvhiMaz <avhimazumder5@outlook.com>
2024-07-13 13:59:37 +0530

    add user model

---
+++ backend/models/user.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { User } from "../types/userTypes";
+
+const userSchema = new mongoose.Schema<User>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  post: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+});
+
+module.exports = mongoose.model<User>("User", userSchema);
---
+++ backend/types/userTypes.ts
@@ -0,0 +1,6 @@
+export interface User {
+  name: string;
+  email: string;
+  password: string;
+  post: string[];
+}
