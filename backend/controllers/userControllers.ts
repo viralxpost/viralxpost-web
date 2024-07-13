835c3ca3e567657cbaeac92f1d9de4a7af284168
AvhiMaz <avhimazumder5@outlook.com>
2024-07-13 21:57:48 +0530

    add a validator for eamil and password

--- backend/controllers/userControllers.ts
+++ backend/controllers/userControllers.ts
@@ -1,55 +1,55 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import createHttpError from "http-errors";
+import validator from "validator";
 import userModel from "../models/user";
 import postModel from "../models/post";
-import { User } from "../types/userTypes";
 import { config } from "../config/config";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
+
   if (!name || !email || !password) {
-    const error = createHttpError(
-      400,
-      "Please provide all the required fields"
+    return next(createHttpError(400, "Please provide all the required fields"));
+  }
+
+  if (!validator.isEmail(email)) {
+    return next(createHttpError(400, "Invalid email format"));
+  }
+
+  const passwordValidation = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+  if (!passwordValidation.test(password)) {
+    return next(
+      createHttpError(
+        400,
+        "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number"
+      )
     );
-    return next(error);
   }
 
   try {
-    const user = await userModel.findOne({ email });
-    if (user) {
-      const error = createHttpError(400, "User already exists");
-      return next(error);
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      return next(createHttpError(400, "User already exists"));
     }
-  } catch (error) {
-    return next(createHttpError(500, "Something went wrong"));
-  }
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  let newUser: User;
-
-  try {
-    newUser = await userModel.create({
+    const newUser = await userModel.create({
       name,
       email,
       password: hashedPassword,
     });
-  } catch (error) {
-    return next(createHttpError(500, "Something went wrong"));
-  }
 
-  try {
     const token = jwt.sign({ sub: newUser._id }, config.jwtSecret as string, {
       expiresIn: "7d",
     });
 
     res.status(201).json({ accessToken: token });
   } catch (error) {
-    return next(createHttpError(500, "Something went wrong"));
+    next(createHttpError(500, "Something went wrong"));
   }
 };
 
-export { createUser };
+export default createUser;
