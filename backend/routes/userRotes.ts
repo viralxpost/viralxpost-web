dde4118febf51db9307d7855ad6e91dc9c50b66c
AvhiMaz <avhimazumder5@outlook.com>
2024-07-14 09:24:45 +0530

    create logout user api

--- backend/routes/userRotes.ts
+++ backend/routes/userRotes.ts
@@ -1,10 +1,15 @@
 import express from "express";
 
-import { createUser, loginUser } from "../controllers/userControllers";
+import {
+  createUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/userControllers";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", createUser);
 userRouter.post("/login", loginUser);
+userRouter.post("/logout", logoutUser);
 
 export default userRouter;
