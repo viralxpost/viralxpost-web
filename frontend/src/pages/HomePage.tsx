1e06370274cffed5cad51ce248a6fd309951135f
PawanK06 <pawan06kumar2003@gmail.com>
2024-07-14 23:34:34 +0530

    add phone image animation

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -55,7 +55,7 @@ const HomePage = () => {
         <div className="items-center mt-8 md:mt-0 justify-center">
           
           <img
-          className="md:hidden block  z-10 transition-opacity delay-[680ms]"
+          className="md:hidden block  z-10 translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:800ms]"
           src="/dark-hero.png"
           alt="Hero image"
         />
