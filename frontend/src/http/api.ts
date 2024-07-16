477f674fbcc1ee958ee5aeaf7a6ae29e331ffb83
AvhiMaz <avhimazumder5@outlook.com>
2024-07-16 13:15:50 +0530

    fix manipulation of link

--- frontend/src/http/api.ts
+++ frontend/src/http/api.ts
@@ -1,13 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5513',
-    headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`,
-        'Content-Type': 'application/json',
-    },
-})
+  baseURL: "http://localhost:5513",
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+    "Content-Type": "application/json",
+  },
+});
 
-export const login = async (data: {email: string, password: string}) => {
-    return api.post('/api/v0/users/login', data)
-}
+export const login = async (data: { email: string; password: string }) => {
+  return api.post("/api/v0/users/login", data);
+};
