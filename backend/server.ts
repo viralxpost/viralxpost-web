5889e79a3953e3fab6d1a6504acb5b1b2bdad900
AvhiMaz <avhimazumder5@outlook.com>
2024-07-16 12:38:41 +0530

    env specific logging

--- backend/server.ts
+++ backend/server.ts
@@ -2,12 +2,16 @@ import app from "./app";
 import { config } from "./config/config";
 import connectDB from "./config/db";
 
-const startServer = async() => {
+const startServer = async () => {
   await connectDB();
   const port = config.port || 3000;
 
   app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    if (config.nodeEnv === "development") {
+      console.log(`Server is running on port ${port}`);
+    } else {
+      console.log("");
+    }
   });
 };
 
