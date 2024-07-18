4991dd434e1b7cc1890d54d0807befea47fa80f1
AvhiMaz <avhimazumder5@outlook.com>
2024-07-18 14:53:04 +0530

    fetch and render all tweets of a user

--- frontend/src/components/TweetCard.tsx
+++ frontend/src/components/TweetCard.tsx
@@ -1,29 +1,28 @@
-
-import { Button } from "@/components/ui/button"
+// src/components/TweetCard.tsx
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-export function TweetCard() {
+} from "@/components/ui/card";
+import { Tweet } from "@/http/api";
+
+interface TweetCardProps {
+  tweet: Tweet;
+}
+
+export function TweetCard({ tweet }: TweetCardProps) {
   return (
     <Card className="">
       <CardHeader>
-        <CardTitle>Common uses of web3 include:
-        </CardTitle>
+        <CardTitle>{tweet.title}</CardTitle>
       </CardHeader>
-      <CardContent>
-      Selling goods and services online with near-zero fees.
-
-Selling digital or physical items, ensuring that each item is genuine.
-
-Instant global payment, without the time and expense of traditional money transfer companies
-      </CardContent>
+      <CardContent>{tweet.content}</CardContent>
       <CardFooter className=" justify-end">
         <Button variant="destructive">Delete</Button>
       </CardFooter>
     </Card>
-  )
+  );
 }
--- frontend/src/pages/Tweets.tsx
+++ frontend/src/pages/Tweets.tsx
@@ -1,13 +1,27 @@
-import { TweetCard } from "@/components/TweetCard"
-import { Button } from "@/components/ui/button"
+import { TweetCard } from "@/components/TweetCard";
+import { Button } from "@/components/ui/button";
 
-import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator } from "@/components/ui/dropdown-menu"
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { getAllTweets, Tweet } from "@/http/api";
+import { useQuery } from "@tanstack/react-query";
 
-import { PlusCircle } from "lucide-react"
+import { PlusCircle } from "lucide-react";
 
 const Tweets = () => {
+  const { data, error, isLoading } = useQuery<{ tweets: Tweet[] }, Error>({
+    queryKey: ["tweets"],
+    queryFn: getAllTweets,
+  });
+
+  console.log(data);
   return (
     <>
       <div className="flex"></div>
@@ -19,48 +33,47 @@ const Tweets = () => {
           <Tabs defaultValue="all">
             <div className="grid">
               <div className="flex items-center justify-between">
+                <TabsList>
+                  <TabsTrigger value="all">All</TabsTrigger>
+                  <TabsTrigger value="active">Active</TabsTrigger>
+                  <TabsTrigger value="draft">Draft</TabsTrigger>
+                </TabsList>
+                <div className="">
+                  <DropdownMenu>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuCheckboxItem checked>
+                        Active
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
 
-              <TabsList>
-                <TabsTrigger value="all">All</TabsTrigger>
-                <TabsTrigger value="active">Active</TabsTrigger>
-                <TabsTrigger value="draft">Draft</TabsTrigger>
-              </TabsList>
-              <div className="">
-                <DropdownMenu>
-                  
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuLabel>Filter by</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuCheckboxItem checked>
-                      Active
-                    </DropdownMenuCheckboxItem>
-                    <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-      
-                <Button size="sm" className="h-8 gap-1">
-                  <PlusCircle className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    Create Tweet
-                  </span>
-                </Button>
-              </div>
+                  <Button size="sm" className="h-8 gap-1">
+                    <PlusCircle className="h-3.5 w-3.5" />
+                    <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                      Create Tweet
+                    </span>
+                  </Button>
+                </div>
               </div>
             </div>
-            <TabsContent value="all">
-
-            </TabsContent>
+            <TabsContent value="all"></TabsContent>
           </Tabs>
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
-          <TweetCard/>
-          <TweetCard/>
-          <TweetCard/>
-          <TweetCard/>
-        </div>
+          <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
+            {data?.tweets && data.tweets.length > 0 ? (
+              data.tweets.map((tweet) => (
+                <TweetCard key={tweet.id} tweet={tweet} />
+              ))
+            ) : (
+              <div>No tweets available</div>
+            )}
+          </div>
         </main>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Tweets
+export default Tweets;
