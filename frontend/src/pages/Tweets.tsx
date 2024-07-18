258d4751acd58e2883b836617e0b391a2518648a
PawanK06 <pawan06kumar2003@gmail.com>
2024-07-18 13:41:42 +0530

    add tweet card

--- frontend/src/pages/Tweets.tsx
+++ frontend/src/pages/Tweets.tsx
@@ -1,26 +1,66 @@
+import { TweetCard } from "@/components/TweetCard"
+import { Button } from "@/components/ui/button"
+
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator } from "@/components/ui/dropdown-menu"
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+import { PlusCircle } from "lucide-react"
 
 const Tweets = () => {
-    return (
-      <>
-        <div className="flex items-center"></div>
-        <div
-          className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-          x-chunk="dashboard-02-chunk-1"
-        >
-          {/* <div className="flex flex-col items-center gap-1 text-center">
-            <h3 className="text-2xl font-bold tracking-tight">
-              Generate Your First Post
-            </h3>
-            <p className="text-sm text-muted-foreground">
-              You can generate your first X post using ai.
-            </p>
-             <Button className="mt-4 flex gap-3">
-              <MagicWandIcon /> Generate
-            </Button>
-          </div> */}
+  return (
+    <>
+      <div className="flex"></div>
+      <div
+        className="flex flex-1 flex-col rounded-lg py-8 border border-dashed shadow-sm"
+        x-chunk="dashboard-02-chunk-1"
+      >
+        <main className="grid items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+          <Tabs defaultValue="all">
+            <div className="grid">
+              <div className="flex items-center justify-between">
+
+              <TabsList>
+                <TabsTrigger value="all">All</TabsTrigger>
+                <TabsTrigger value="active">Active</TabsTrigger>
+                <TabsTrigger value="draft">Draft</TabsTrigger>
+              </TabsList>
+              <div className="">
+                <DropdownMenu>
+                  
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuCheckboxItem checked>
+                      Active
+                    </DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+      
+                <Button size="sm" className="h-8 gap-1">
+                  <PlusCircle className="h-3.5 w-3.5" />
+                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                    Create Tweet
+                  </span>
+                </Button>
+              </div>
+              </div>
+            </div>
+            <TabsContent value="all">
+
+            </TabsContent>
+          </Tabs>
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
+          <TweetCard/>
+          <TweetCard/>
+          <TweetCard/>
+          <TweetCard/>
         </div>
-      </>
-    )
-  }
-  
-  export default Tweets
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default Tweets
