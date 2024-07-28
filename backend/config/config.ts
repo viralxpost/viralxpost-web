2236d46476e16af80263573803ebf113263b8eb2
PawanK06 <pawan06kumar2003@gmail.com>
2024-07-28 22:54:52 +0530

    add urls

--- backend/config/config.ts
+++ backend/config/config.ts
@@ -7,10 +7,18 @@ const _config = {
   jwtSecret: process.env.JWT_SECRET,
   nodeEnv: process.env.NODE_ENV,
   geminiApi: process.env.GEMINI_API,
-  frontendDomain: process.env.FRONTEND_DOMAIN || "https://www.viralxpost.xyz" || "https://viralxpost.xyz", 
+  frontendDomain:
+    process.env.FRONTEND_DOMAIN ||
+    "https://www.viralxpost.xyz" ||
+    "https://viralxpost.xyz",
   redisHost: process.env.REDIS_SOCKET_HOST,
   redisPort: process.env.REDIS_PORT,
   redisPassword: process.env.REDIS_PASSWORD,
+  expressSessionSecret: process.env.EXPRESS_SECRET,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleSecretKey: process.env.GOOGLE_SECRET_KEY,
+  expressSecret: process.env.EXPRESS_SECRET,
+  callbackUrl: process.env.GOOGLE_CALLBACK_URL
 };
 
 export const config = Object.freeze(_config);
