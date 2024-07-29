46f4937323e26bc4d362cfd0ea921127d37bd8dd
PawanK06 <pawan06kumar2003@gmail.com>
2024-07-29 16:51:30 +0530

    add some input changes in accordian

--- frontend/src/components/ui/accordion.tsx
+++ frontend/src/components/ui/accordion.tsx
@@ -13,7 +13,7 @@ const AccordionItem = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AccordionPrimitive.Item
     ref={ref}
-    className={cn("border-b border-[#4B4848] border-opacity-40", className)}
+    className={cn("border-b border-[#4B4848] border-opacity-20 dark:border-opacity-30", className)}
     {...props}
   />
 ))
