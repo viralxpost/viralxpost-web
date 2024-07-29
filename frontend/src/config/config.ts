ddcb5fe4a0e564aabdaf648effc788e2700c2ac3
AvhiMaz <avhimazumder5@outlook.com>
2024-07-29 17:19:24 +0530

    setup config in frontend

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,6 +1,7 @@
 const config = {
   isDevelopment: import.meta.env.VITE_APP_ENV === "development",
   backendBaseUrl: import.meta.env.VITE_BACKEND_BASE_URL as string,
+  razorPay: import.meta.env.VITE_RAZORPAY_KEY_ID
 };
 
 export default config;
