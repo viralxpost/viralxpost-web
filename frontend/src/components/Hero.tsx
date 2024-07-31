94351f7a50171860ce1ddbcb7b5d49f5abc76c1e
PawanK06 <pawan06kumar2003@gmail.com>
2024-07-31 12:09:15 +0530

    add hero section

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -11,14 +11,14 @@ interface HeroElementProps {
 export const HeroTitle = ({ children, className }: HeroElementProps) => {
     return (
     <>
-    <h1 className={cn(`md:text-5xl lg:text-6xl font-semibold  text-lg my-4 ${className}`)}>{children}</h1>
+    <h1 className={cn(`md:text-5xl lg:text-6xl font-semibold section-title mt-5 ${className}`)}>{children}</h1>
     </>
     )
 }
 
 export const HeroSubtitle = ({children, className}: HeroElementProps) => {
     return (
-        <p className={cn('md:text-lg text-[10px] mb-6 opacity-40', className)}>{children}</p>
+        <p className={cn('md:text-lg mb-6 opacity-40 section-description mt-5', className)}>{children}</p>
     )
 }
 
