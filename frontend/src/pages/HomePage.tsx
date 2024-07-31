0a29d5865e4037c7303046ed69e83eb71f95bf18
PawanK06 <pawan06kumar2003@gmail.com>
2024-07-31 12:08:57 +0530

    worked on landing page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,51 +2,46 @@ import { AvatarCirclesUrl } from "@/components/Avatarurl";
 import BentoComp from "@/components/BentoComp";
 import Container from "@/components/Container";
 import Faq from "@/components/Faq";
-import { Hero, HeroSubtitle, HeroTitle } from "@/components/Hero";
 import { HeroImage } from "@/components/HeroImage";
 import LogoTicker from "@/components/LogoTicker";
 import { MarqueeComp } from "@/components/MarqueeComp";
-import AnimatedShinyText from "@/components/magicui/animated-shiny-text";
+import Pricing from "@/components/Pricing";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import { ArrowRightIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
- 
+
   return (
-    <Container>      
-      <div className="z-10 translate-y-[-1rem] mt-24 animate-fade-in opacity-0 [--animation-delay:100ms] flex lg:min-h-[10rem] md:min-h-[7rem] min-h-[5rem] items-center justify-center">
-        <div
-          className={cn(
-            "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-zinc-900 dark:hover:bg-neutral-800"
-          )}
-        >
-          <AnimatedShinyText className="inline-flex md:text-sm text-xs items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600  hover:duration-300 hover:dark:text-neutral-400">
-            <span>✨ Introducing viralxpost AI</span>
-            <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
-          </AnimatedShinyText>
+    <Container>
+      <div className="z-10 translate-y-[-1rem] mt-10 animate-fade-in opacity-0 [--animation-delay:100ms] flex lg:min-h-[10rem] md:min-h-[7rem] min-h-[5rem] items-center justify-center">
+        <div className="mt-28  max-w-[1000px] mx-auto">
+          <div className="section-container">
+          </div>
+          <div className="flex justify-center">
+            <div className="tag">Version 1.0 is here</div>
+          </div>
+
+          <div className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms] section-title mt-5">
+            Start Generating Viral Tweets Today{" "}
+            <br className="hidden md:inline-block " /> Let{" "}
+            <Link to="/" className="underline">
+              viralxpost
+            </Link>{" "}
+            Be Your Guide.
+          </div>
+          <div className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms] section-description mt-5">
+            Let our AI tool help you create engaging content that breaks{" "}
+            <br className="hidden md:block" /> through the noise and gets you seen
+            by the right people.
+          </div>
+          <Link to="/auth/login" className="flex justify-center items-center">
+            <Button className="translate-y-[-1rem] mt-5 animate-fade-in opacity-0 [--animation-delay:600ms]">
+              Get Started For Free
+            </Button>
+          </Link>
         </div>
       </div>
-      <Hero>
-        <HeroTitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
-          Start Generating Viral Tweets Today{" "}
-          <br className="hidden md:inline-block " /> Let{" "}
-          <Link to="/" className="underline">
-            viralxpost
-          </Link>{" "}
-          Be Your Guide.
-        </HeroTitle>
-        <HeroSubtitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
-          Let our AI tool help you create engaging content that breaks{" "}
-          <br className="hidden md:block" /> through the noise and gets you seen
-          by the right people.
-        </HeroSubtitle>
-        <Link to="/auth/login">
-          <Button className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:600ms]">
-            Get Started For Free
-          </Button>
-        </Link>
+      <div>
 
         <div className="flex items-center justify-center">
           <div className="hidden md:block  glow absolute bottom-64 -z-10 aspect-square  w-full h-full max-h-64 max-w-4xl rounded-full bg-blue-400/15 dark:bg-[#737373] opacity-10  blur-3xl filter" />
@@ -60,21 +55,34 @@ const HomePage = () => {
             alt="Hero image"
           />
         </div>
-        <div className="flex justify-center mt-20 w-96 mx-auto"></div>
+        <div className="mt-28  max-w-[1000px] mx-auto">
 
-        <LogoTicker />
+          <LogoTicker />
+
+        </div>
+
         <BentoComp />
         <div className="mt-28  max-w-[1000px] mx-auto">
-          <HeroTitle>Trusted by the best</HeroTitle>
+          <div className="section-container">
+
+            <div className="flex justify-center">
+              <div className="tag">Testimonials</div>
+            </div>
+            <h2 className="section-title mt-5">What our users say</h2>
+            <p className="section-description mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi molestias ipsum commodi!
+            </p>
+          </div>
           <div className="py-6">
             <AvatarCirclesUrl />
           </div>
 
           <MarqueeComp />
         </div>
+        <Pricing />
 
         <Faq />
-      </Hero>
+      </div>
     </Container>
   );
 };
