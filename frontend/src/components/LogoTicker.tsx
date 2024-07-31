095964d2254b3d2a9bfbc9da0845b491b40dbf5b
PawanK06 <pawan06kumar2003@gmail.com>
2024-07-31 12:09:25 +0530

    add logo ticket

--- frontend/src/components/LogoTicker.tsx
+++ frontend/src/components/LogoTicker.tsx
@@ -1,47 +1,35 @@
-
-import acmeLogo from "../assets/images/acme.png";
-import quantumLogo from "../assets/images/quantum.png";
-import echoLogo from "../assets/images/echo.png";
-import celestialLogo from "../assets/images/celestial.png";
-import pulseLogo from "../assets/images/pulse.png";
-import apexLogo from "../assets/images/apex.png";
+import acmeLogo from '@/assets/logo-acme.png'
+import quantumLogo from '@/assets/logo-quantum.png'
+import echoLogo from '@/assets/logo-echo.png'
+import celestialLogo from '@/assets/logo-celestial.png'
+import pulseLogo from '@/assets/logo-pulse.png'
 import { motion } from 'framer-motion'
-
-const images = [
-  { src: acmeLogo, alt: "Acme Logo" },
-  { src: quantumLogo, alt: "Quantum Logo" },
-  { src: echoLogo, alt: "Echo Logo" },
-  { src: celestialLogo, alt: "Celestial Logo" },
-  { src: pulseLogo, alt: "Pulse Logo" },
-  { src: apexLogo, alt: "Apex Logo" },
-];
 
 const LogoTicker = () => {
   return (
-    <div className=" text-white py-[72px]">
-      <div className="container ">
-
-        <div className=" flex overflow-hidden before:content-[''] before:z-10 after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative  after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
-          <motion.div
-            transition={{
-              duration: 10,
-              ease: "linear",
-              repeat: Infinity
-            }}
-            initial={{ translateX: 0 }}
-            animate={{ translateX: '-50%' }} className="flex gap-16 flex-none pr-16">
-
-            {
-              images.map(image => (
-                <img src={image.src} key={image.alt} alt={image.alt} className="flex-none h-8 w-auto" />
-              ))
-            }
-
-            {
-              images.map(image => (
-                <img src={image.src} key={image.alt} alt={image.alt} className="flex-none h-8 w-auto" />
-              ))
-            }
+    <div className='my-10 bg-white'>
+      <div className='px-5 md:px-10 md:flex items-center justify-center'>
+        <div className='flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]'>
+          <motion.div className='flex gap-14 flex-none pr-14' animate={{
+            translateX: '-50%',
+          }}
+          transition={{
+            duration: 20, 
+            repeat: Infinity,
+            ease: "linear",
+            repeatType: "loop"
+          }}
+          >
+            <img src={acmeLogo} alt="Acme Logo" className='logo-ticker-img' />
+            <img src={quantumLogo} alt="Quantum Logo" className='logo-ticker-img' />
+            <img src={echoLogo} alt="Echo Logo" className='logo-ticker-img' />
+            <img src={celestialLogo} alt="Celestial Logo" className='logo-ticker-img' />
+            <img src={pulseLogo} alt="Pulse Logo" className='logo-ticker-img' />
+            <img src={acmeLogo} alt="Acme Logo" className='logo-ticker-img' />
+            <img src={quantumLogo} alt="Quantum Logo" className='logo-ticker-img' />
+            <img src={echoLogo} alt="Echo Logo" className='logo-ticker-img' />
+            <img src={celestialLogo} alt="Celestial Logo" className='logo-ticker-img' />
+            <img src={pulseLogo} alt="Pulse Logo" className='logo-ticker-img' />
           </motion.div>
         </div>
       </div>
@@ -50,4 +38,3 @@ const LogoTicker = () => {
 }
 
 export default LogoTicker
-
