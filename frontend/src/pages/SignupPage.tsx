b69081a19132d0982e48fc8f2c01f578ef08ec43
PawanK06 <pawan06kumar2003@gmail.com>
2024-08-01 14:22:14 +0530

    add singup changes

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,24 +1,14 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import config from "@/config/config";
-import { register } from "@/http/api";
-import { cn } from "@/lib/utils";
 import useTokenStore from "@/store";
-import { useMutation } from "@tanstack/react-query";
-import { Loader } from "lucide-react";
 
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const SignupPage = () => {
   const navigate = useNavigate();
 
   const setToken = useTokenStore((state) => state.setToken);
-
-  const nameRef = useRef<HTMLInputElement>(null);
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -33,35 +23,7 @@ const SignupPage = () => {
     }
   }, [navigate, setToken]);
 
-  const mutation = useMutation({
-    mutationFn: register,
-    onSuccess: (response) => {
-      if (config.isDevelopment) {
-        console.log("signup successful");
-      }
-      setToken(response.accessToken);
-      //redirect to dashboard
-      navigate("/dashboard");
-    },
-    onError: (error) => {
-      if (config.isDevelopment) {
-        console.log("Error:", error);
-      }
-    },
-  });
 
-  const handleSignupSubmit = () => {
-    const name = nameRef.current?.value;
-    const email = emailRef.current?.value;
-    const password = passwordRef.current?.value;
-
-    //make server call
-    if (!name || !email || !password) {
-      return alert("Please enter all fields");
-    }
-
-    mutation.mutate({ name, email, password });
-  };
 
   const handleGoogleSignUp = () => {
     window.location.href = `${config.backendBaseUrl}auth/google`;
@@ -73,62 +35,9 @@ const SignupPage = () => {
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
             <h1 className="text-3xl font-bold">Sign up</h1>
-            <p className="text-sm text-muted-foreground">
-              Enter your username, email and password below to register to your
-              account
-              <br />
-              {mutation.isError && (
-                <span className="text-red-500 text-sm">
-                  {mutation.error?.message || "An error occurred"}
-                </span>
-              )}
-            </p>
           </div>
           <div className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="name">Name</Label>
-              <Input
-                ref={nameRef}
-                id="name"
-                type="name"
-                placeholder="m"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                ref={emailRef}
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link
-                  to="/forgot-password"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  Forgot your password?
-                </Link>
-              </div>
-              <Input ref={passwordRef} id="password" type="password" required />
-            </div>
-            <Button
-              onClick={handleSignupSubmit}
-              type="submit"
-              className="w-full"
-            >
-              <Loader
-                className={cn(mutation.isPending ? "animate-spin" : "hidden")}
-              />
-              <span className={cn(mutation.isPending ? "hidden" : "block")}>
-                Sign up
-              </span>
-            </Button>
+      
             <Button
               variant="outline"
               className="w-full"
