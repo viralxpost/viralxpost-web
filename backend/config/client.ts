4c37d75700e1705e111435e49831893325c960ba
AvhiMaz <avhimazumder5@outlook.com>
2024-08-01 11:32:35 +0530

    redis code change

--- backend/config/client.ts
+++ backend/config/client.ts
@@ -2,18 +2,35 @@ import { createClient } from "redis";
 import { config } from "../config/config";
 
 // Redis client setup
-
 const client = createClient({
   password: config.redisPassword,
   socket: {
     host: config.redisHost,
     port: Number(config.redisPort),
-    connectTimeout: 10000,
+    connectTimeout: 20000,
   },
 });
 
-client.connect();
+const connectWithRetry = async (retries: number = 5) => {
+  for (let i = 0; i < retries; i++) {
+    try {
+      await client.connect();
+      console.log("Connected to Redis server successfully");
+      break;
+    } catch (error) {
+      console.error("Redis connection attempt failed:", error);
+      if (i < retries - 1) {
+        console.log(`Retrying connection (${i + 1}/${retries})...`);
+        await new Promise((res) => setTimeout(res, 2000));
+      } else {
+        console.error("Failed to connect to Redis after multiple attempts");
+      }
+    }
+  }
+};
 
 client.on("error", (err: string) => console.error("Redis Client Error", err));
+
+connectWithRetry();
 
 export default client;
