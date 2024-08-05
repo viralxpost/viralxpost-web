bb8c6b57b9aaa230987ada0c9d1cbcd310e64781
AvhiMaz <avhimazumder5@outlook.com>
2024-08-05 20:49:43 +0530

    edit both frontend and backend installation script

--- install-frontend.sh
+++ install-frontend.sh
@@ -1,3 +1,7 @@
-cd frontend
-npm i
-npm run dev
+#!/bin/bash
+
+    echo "Installing frontend dependencies..."
+    cd frontend || exit
+    npm install
+    echo "Starting frontend..."
+    npm run dev
