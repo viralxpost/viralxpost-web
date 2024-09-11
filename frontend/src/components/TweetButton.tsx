7c6e715cf3bfc9eb4acea5aaed4981864a755bee
AvhiMaz <avhimazumder5@outlook.com>
2024-09-11 17:33:53 +0530

    change twitter logo

--- frontend/src/components/TweetButton.tsx
+++ frontend/src/components/TweetButton.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { TwitterLogoIcon } from "@radix-ui/react-icons";
+import { FaXTwitter } from "react-icons/fa6";
 
 const TweetButton = ({ tweetContent }: { tweetContent: string }) => {
   const handleTweet = () => {
@@ -10,7 +10,7 @@ const TweetButton = ({ tweetContent }: { tweetContent: string }) => {
 
   return (
     <Button onClick={handleTweet} className="flex items-center gap-2">
-      <TwitterLogoIcon className="h-7 w-7"/>
+      <FaXTwitter className="h-6 w-6" />
     </Button>
   );
 };
